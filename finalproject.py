cd9fe877361b3b4a189f2d30a7e7693f993cdade
timwiesner <timothyjwiesner@gmail.com>
2017-11-05 20:53:24 -0600

    feat: Begin work on home page render template

--- finalproject.py
+++ finalproject.py
@@ -10,7 +10,6 @@ restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
 
 restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
 
-
 #Fake Menu Items
 items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
 item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
@@ -30,7 +29,7 @@ def showRestaurants():
 # 2. /restaurant/new
 @app.route('/restaurant/new/')
 def newRestaurant():
-    return "This page will be for making a new restaurant"
+    return render_template('newRestaurant.html')
 
 # 3. /restaurant/restaurant_id/edit
 @app.route('/restaurant/<int:restaurant_id>/edit/')
