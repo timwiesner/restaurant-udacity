8e2740d697d166cba6e7bb33f1b0d6ce7be43a6a
timwiesner <timothyjwiesner@gmail.com>
2017-11-05 20:48:18 -0600

    Initial commit

---
+++ finalproject.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+app = Flask(__name__)
+
+#Fake Restaurants
+restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+
+restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+
+
+#Fake Menu Items
+items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+
+# engine = create_engine('sqlite:///restaurantmenu.db')
+# Base.metadata.bind = engine
+
+# DBSession = sessionmaker(bind=engine)
+# session = DBSession()
+
+# 1. /, /restaurants
+@app.route('/')
+@app.route('/restaurants/')
+def showRestaurants():
+    return render_template('restaurants.html', restaurants = restaurants)
+
+# 2. /restaurant/new
+@app.route('/restaurant/new/')
+def newRestaurant():
+    return "This page will be for making a new restaurant"
+
+# 3. /restaurant/restaurant_id/edit
+@app.route('/restaurant/<int:restaurant_id>/edit/')
+def editRestaurant(restaurant_id):
+    return "This page will be for editing restaurant %s" % restaurant_id
+
+# 4. /restaurant/restaurant_id/delete
+@app.route('/restaurant/<int:restaurant_id>/delete/')
+def deleteRestaurant(restaurant_id):
+    return "This page will be for deleting restaurant %s" % restaurant_id
+
+# 5. /restaurant/restaurant_id, /restaurant/restaurant_id/menu
+@app.route('/restaurant/<int:restaurant_id>/')
+@app.route('/restaurant/<int:restaurant_id>/menu/')
+def showMenu(restaurant_id):
+    return "This page is the menu for restaurant %s" % restaurant_id
+
+# 6. /restaurant/restaurant_id/menu/new
+@app.route('/restaurant/<int:restaurant_id>/menu/new')
+def newMenuItem(restaurant_id):
+    return "This page is for making a new menu item for restaurant %s" % restaurant_id
+
+# 7. /restaurant/restaurant_id/menu/menu_id/edit
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+    return "This page is for editing menu item %s" % menu_id
+
+# 8. /restaurant/restaurant_id/menu/menu_id/delete
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "This page is for deleting menu item %s" % menu_id
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
